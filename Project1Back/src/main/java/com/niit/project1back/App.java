b36bbcf98f0764874094a64ea60433d4a943a898
= <=dddey.09@gmail.com>
2016-11-23 14:25:11 +0530

    this is our first commit

---
+++ Project1Back/src/main/java/com/niit/project1back/App.java
@@ -0,0 +1,13 @@
+package com.niit.project1back;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
